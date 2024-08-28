ada2b7d6a76ba4ad040a3f6ff9da024bd32729f8
AliZaminShigri <zaminali7865@gmail.com>
2024-08-28 08:18:08 -0700

    First commit

---
+++ test.py
@@ -0,0 +1,13 @@
+eng = 50
+math = 78
+physics = 80
+average = (eng+math+physics)/3
+print("Average is ", average)
+percentage = (eng+math+physics)/300*100
+print("Percantage is ", percentage)
+print("----------------------------")
+print("Sub      obt marks       total")
+print("----------------------------")
+print("eng      ",eng,"     100")
+print("math      ",math,"     100")
+print("physics      ",physics,"     100")
